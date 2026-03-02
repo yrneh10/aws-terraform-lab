cf8ec88da3223d1e17f2d39db24fe289c90f359e
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 11:06:57 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
+# AWS account and region
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
+# VPC
 variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
@@ -22,20 +29,42 @@ variable "public_subnet2_cidr" {
   default     = "10.0.2.0/24"
 }
 
+# ALB
 variable "alb_name" {
   description = "Application Load Balancer name"
   type        = string
   default     = "devsecops-alb"
 }
 
+# ECS
 variable "ecs_cluster_name" {
   description = "ECS Cluster name"
   type        = string
   default     = "devsecops-cluster"
 }
 
+# ECR
 variable "ecr_repo_name" {
   description = "ECR repository name"
   type        = string
   default     = "devsecops-app"
+}
+
+# ECS task
+variable "ecs_task_cpu" {
+  description = "CPU for ECS task"
+  type        = string
+  default     = "256"
+}
+
+variable "ecs_task_memory" {
+  description = "Memory for ECS task"
+  type        = string
+  default     = "512"
+}
+
+variable "ecs_desired_count" {
+  description = "Number of ECS tasks"
+  type        = number
+  default     = 1
 }
