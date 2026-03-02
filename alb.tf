dce1d445caa2a492c6029e24de702f04149c8995
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 11:08:54 +0800

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -7,10 +7,10 @@ resource "aws_lb" "app_lb" {
 }
 
 resource "aws_lb_target_group" "app_tg" {
-  name     = "devsecops-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${var.alb_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 }
 
