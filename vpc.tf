76712ea362c5b81e2694b89bda2874607ba77a12
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 11:02:24 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_subnet" "public1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.public_subnet1_cidr
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -27,7 +27,7 @@ resource "aws_subnet" "public1" {
 
 resource "aws_subnet" "public2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = var.public_subnet2_cidr
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 
