105917c5ed88d473d4f584ce14f4287342df22e3
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 11:05:16 +0800

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "app_lb" {
-  name               = "devsecops-alb"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
