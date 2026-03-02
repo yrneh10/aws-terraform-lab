bc457eff0bfa452a71ecc5d5faf1608872ebce32
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 10:58:52 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,41 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet1_cidr" {
+  description = "Public subnet 1 CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2_cidr" {
+  description = "Public subnet 2 CIDR"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "alb_name" {
+  description = "Application Load Balancer name"
+  type        = string
+  default     = "devsecops-alb"
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS Cluster name"
+  type        = string
+  default     = "devsecops-cluster"
+}
+
+variable "ecr_repo_name" {
+  description = "ECR repository name"
+  type        = string
+  default     = "devsecops-app"
 }
