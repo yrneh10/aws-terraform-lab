b22c424b14e63b6ec2527f38639407498092ed88
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-02 10:53:31 +0800

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,26 @@
+resource "aws_lb" "app_lb" {
+  name               = "devsecops-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "devsecops-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
