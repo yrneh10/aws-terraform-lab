c76f883ca44109a9f69010bda5d189c78dc6d9f7
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2026-03-03 09:33:03 +0800

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -40,5 +40,11 @@ resource "aws_ecs_service" "app" {
     assign_public_ip = true
   }
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
   depends_on = [aws_lb_listener.http_listener]
 }
